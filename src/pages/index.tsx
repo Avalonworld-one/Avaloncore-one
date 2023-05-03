881b30d6d18a2f8e1befd69b79a5b97239f7a245
Paigeyuan <puttida.yu@gmail.com>
2023-05-04 04:46:14 +0700

    Update GitHub workflow and modify pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,12 @@
 // src/pages/index.tsx
 import React from 'react';
-import Header from '../components/Header';
 import Introduction from '../components/Introduction';
 import ProductSection from '../components/ProductSection';
 import AvalonFeatures from '../components/AvalonFeatures';
 
-
-import { GetServerSideProps } from 'next';
-
-interface HomepageProps {
-  // data: any; // Replace 'any' with the appropriate data type
-}
-
-const Homepage: React.FC<HomepageProps> = () => {
+const Homepage: React.FC = () => {
   return (
     <div>
-      <Header />
       <main>
         <Introduction />
         <ProductSection />
@@ -24,16 +15,6 @@ const Homepage: React.FC<HomepageProps> = () => {
       </main>
     </div>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  // const data = await fetchData();
-
-  return {
-    props: {
-      // data,
-    },
-  };
 };
 
 export default Homepage;
